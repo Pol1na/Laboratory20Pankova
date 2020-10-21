635cbb5d0bfb80201b544a93c3e896054e90b1ef
Polina Hehe <l1napochtipol1na@gmail.com>
2020-10-21 18:50:52 +0300

    auth page and frame + need to fix user's pages

---
+++ Laboratory/Laboratory/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laboratory
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new Pages.AuthPage());
+        }
+
+
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                MainFrame.GoBack();
+            }
+        }
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            var page = (sender as Frame).Content as Page;
+
+            if(page is Pages.AuthPage)
+            {
+                BackBtn.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                BackBtn.Visibility = Visibility.Visible;
+            }
+
+        }
+    }
+}
